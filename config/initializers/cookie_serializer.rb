d4a04aab35b67337f06d6c74bae991297a572a32
Ronaldo-Martinez <irmv_0311@hotmail.com>
2023-06-15 06:57:07 +0000

    cookie _authentication_app_2, en local

--- config/initializers/cookie_serializer.rb
+++ config/initializers/cookie_serializer.rb
@@ -1,5 +1,10 @@
 #config/initializers/cookie_serailizer.rb
 Rails.application.config.action_dispatch.cookies_serializer = :hybrid
 #config/initializers/session_store.rb
-Rails.application.config.session_store :cookie_store, key: '_authentication_app_2', same_site: :none, secure: "true"
+if 'true'===ENV['SSL']
+    Rails.application.config.session_store :cookie_store, key: '_authentication_app_2', same_site: :none, secure: "true"
+else
+    Rails.application.config.session_store :cookie_store, key: '_authentication_app_2'
+end
+
 
